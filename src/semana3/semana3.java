1af328a8f38c697722bd2f8c861840e85511b6d2
jorgebastos <jorgepbastos8@gmail.com>
2015-12-08 22:49:44 +0000

    semana3-faltam testes finais

--- src/semana3/semana3.java
+++ src/semana3/semana3.java
@@ -1,8 +1,11 @@
 package semana3;
 
 import javax.crypto.*;
-import javax.crypto.spec.SecretKeySpec;
-import java.io.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,43 +15,84 @@ import java.security.cert.CertificateException;
 /**
  * Created by jorge on 02-10-2015.
  */
-public class semana3 {
+public class semana3 { //faltam testes finais
 
-    //private static char[] password = "password".toCharArray();
 
-    public static void main(String[] args ) throws IOException, NoSuchAlgorithmException { //TODO
+    public static void main(String[] args) throws Exception { //TODO
         SecretKey key;
         Path in_file;
         Path out_file;
-        Path k_path;
+        String k_path;
+        String k_password;
+        KeyGenerator kg;
 
 
-        System.out.println("hello");
-        if(args.length==2 || args.length==4)
-        {
-            if(args[0].equals("-genkey"))
-            {
-                k_path = Paths.get(args[1]);
-                genKey(k_path);
-            }else if (args.length == 4) {
+        if (args.length == 3 || args.length == 5) {
+            if (args[0].equals("-genkey")) {
+                byte[] fkey;
+                k_path = args[1];
+                k_password = args[2];
+                KeyStore.ProtectionParameter passwordProtection = new KeyStore.PasswordProtection(k_password.toCharArray());
+
+                kg = genKey();
+                key = kg.generateKey();
+                fkey = key.getEncoded();
+
+                KeyStore keyStore = createKS(k_path, k_password);
+                KeyStore.SecretKeyEntry skentry = new KeyStore.SecretKeyEntry(key);
+                keyStore.setEntry("keystore", skentry, passwordProtection);
+                keyStore.store(new FileOutputStream(k_path), k_password.toCharArray());
+
+
+            } else if (args.length == 5) {
                 if (args[0].equals("-enc")) {
-                    k_path = Paths.get(args[1]);
-                    in_file = Paths.get(args[2]);
-                    out_file = Paths.get(args[3]);
-                    key = readKey(k_path);
-                    encFile(key, in_file, out_file);
+                    k_path = args[1];
+                    k_password = args[2];
+                    in_file = Paths.get(args[3]);
+                    out_file = Paths.get(args[4]);
+                    KeyStore.ProtectionParameter passwordProtection = new KeyStore.PasswordProtection(k_password.toCharArray());
+                    KeyStore keyStore = createKS(k_path, k_password);
+                    KeyStore.Entry entry = keyStore.getEntry("keystore",passwordProtection);
+                    key = ((KeyStore.SecretKeyEntry) entry).getSecretKey();
+
+                    //inicializa mac
+                    Mac mac = Mac.getInstance("Hmac-SHA-1");
+                    mac.init(key);
+
+                    //vector de inicializacao
+                    SecureRandom r = new SecureRandom();
+                    byte[] iv = new byte[16];
+                    r.nextBytes(iv);
+                    IvParameterSpec ivSpec=new IvParameterSpec(iv);
+                    encFile(key, in_file, out_file, ivSpec,mac);
                 }
                 if (args[0].equals("-dec")) {
-                    k_path = Paths.get(args[1]);
-                    in_file = Paths.get(args[2]);
-                    out_file = Paths.get(args[3]);
-                    key = readKey(k_path);
-                    decFile(key, in_file, out_file);
-                }else{
+                    k_path = args[1];
+                    k_password = args[2];
+                    in_file = Paths.get(args[3]);
+                    out_file = Paths.get(args[4]);
+                    KeyStore.ProtectionParameter passwordProtection = new KeyStore.PasswordProtection(k_password.toCharArray());
+                    KeyStore keyStore = createKS(k_path, k_password);
+                    KeyStore.Entry entry = keyStore.getEntry("keystore",passwordProtection);
+                    key = ((KeyStore.SecretKeyEntry) entry).getSecretKey();
+
+                    //inicializa mac
+                    Mac mac = Mac.getInstance("Hmac-SHA-1");
+                    mac.init(key);
+
+                    //vector de inicializacao
+                    SecureRandom r = new SecureRandom();
+                    byte[] iv = new byte[16];
+                    r.nextBytes(iv);
+                    IvParameterSpec ivSpec=new IvParameterSpec(iv);
+
+                    decFile(key, in_file, out_file, ivSpec,mac);
+
+                } else {
                     System.out.println("How to use:");
-                    System.out.println("prog -genkey <keyfile>");
-                    System.out.println("prog -enc <keyfile> <infile> <outfile>");
-                    System.out.println("prog -dec <keyfile> <infile> <outfile>");
+                    System.out.println("prog -genkey <keyfile> <password>");
+                    System.out.println("prog -enc <keyfile> <password> <infile> <outfile>");
+                    System.out.println("prog -dec <keyfile> <password> <infile> <outfile>");
                 }
 
 
@@ -58,17 +102,18 @@ public class semana3 {
 
 
 
-    public static void encFile(SecretKey key, Path inputFile, Path outputFile) {
+    public static void encFile(SecretKey key, Path inputFile, Path outputFile, IvParameterSpec ivspec, Mac mac) throws Exception{
         byte[] inf;
         byte[] encf;
 
         try {
             inf = Files.readAllBytes(inputFile);
-            Cipher e = Cipher.getInstance("RC4"); //cria instancia da cifra
+            Cipher e = Cipher.getInstance("AES/CBC/PKCS5Padding"); //cria instancia da cifra
             //inicializa cifra
-            e.init(Cipher.ENCRYPT_MODE, key);
+            e.init(Cipher.ENCRYPT_MODE, key, ivspec);
             encf = e.doFinal(inf); // guarda em encf o array encriptado
-            Files.write(outputFile, encf);
+            byte[] last =mac.doFinal(encf);
+            Files.write(outputFile, last);
 
         } catch (IOException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException e) {
             e.printStackTrace();
@@ -76,17 +121,19 @@ public class semana3 {
 
     }
 
-    public static void decFile(SecretKey key, Path inputFile, Path outputFile) {
+
+    public static void decFile(SecretKey key, Path inputFile, Path outputFile, IvParameterSpec ivspec, Mac mac) throws InvalidAlgorithmParameterException {
         byte[] inf;
         byte[] ffile;
 
         try {
             inf = Files.readAllBytes(inputFile);
-            Cipher e = Cipher.getInstance("RC4"); //cria instancia da cifra
+            Cipher e = Cipher.getInstance("AES/CBC/PKCS5Padding"); //cria instancia da cifra
             //inicializa cifra
-            e.init(Cipher.DECRYPT_MODE, key);
+            e.init(Cipher.DECRYPT_MODE, key, ivspec);
             ffile = e.doFinal(inf);// guarda em ffile a mensagem original já desencriptada
-            Files.write(outputFile, ffile);
+            byte[] last = mac.doFinal(ffile);
+            Files.write(outputFile, last);
         } catch (IOException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException e) {
             e.printStackTrace();
         }
@@ -94,61 +141,26 @@ public class semana3 {
 
     }
 
-    public static void genKey(Path kPath) throws IOException, NoSuchAlgorithmException {
-        byte[] fkey;
+
+    public static KeyGenerator genKey() throws IOException, NoSuchAlgorithmException {
         KeyGenerator kg; // cria instancia do gerador de chaves
-        kg = KeyGenerator.getInstance("RC4");
+        kg = KeyGenerator.getInstance("AES");
         kg.init(128);
-        SecretKey key = kg.generateKey();
-        fkey = key.getEncoded();
-        Files.write(kPath, fkey);
-
-    }
-        //TODO
-    public static SecretKey readKey(Path kPath) throws IOException {
-        byte[] fkey; //128 bits
-        fkey= Files.readAllBytes(kPath);
-        SecretKey fk = new SecretKeySpec(fkey,"RC4");
-        return fk;
-
+        return kg;
     }
 
 
-    public static KeyStore createKS(String fkey, String pass ) throws IOException, KeyStoreException, CertificateException, NoSuchAlgorithmException {
+    public static KeyStore createKS(String fkey, String pass) throws IOException, KeyStoreException, CertificateException, NoSuchAlgorithmException {
         File file = new File(fkey);
         KeyStore ks = KeyStore.getInstance("JCEKS");
-        if(file.exists()){
+        if (file.exists()) {
             ks.load(new FileInputStream(file), pass.toCharArray());
-        }
-        else{
-            ks.load(null,null);
+        } else {
+            ks.load(null, null);
+            ks.store(new FileOutputStream(fkey), pass.toCharArray());
         }
         return ks;
     }
-
-    public static void storeKS(KeyStore ks, String outfile, SecretKey skey, String pass ) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
-        KeyStore.SecretKeyEntry skEntry =
-                new KeyStore.SecretKeyEntry(skey);
-        char[] myPassword = pass.toCharArray();
-        KeyStore.ProtectionParameter protParam = new KeyStore.PasswordProtection(myPassword);
-        ks.setEntry("SecretKeyAlias", skEntry, protParam);
-        ks.store(new FileOutputStream(outfile),myPassword);
-
-    }
-
-    public static SecretKey loadKey(String infile,String pass) throws UnrecoverableEntryException, NoSuchAlgorithmException, KeyStoreException, IOException, CertificateException {
-        char[] keyPass = pass.toCharArray();
-        KeyStore ks = KeyStore.getInstance("JCEKS");
-        ks.load(new FileInputStream(infile),keyPass);
-        KeyStore.ProtectionParameter protParam = new KeyStore.PasswordProtection(keyPass);
-
-        KeyStore.SecretKeyEntry skEntry = (KeyStore.SecretKeyEntry)
-                ks.getEntry("SecretKeyAlias", protParam);
-        SecretKey myKey = skEntry.getSecretKey();
-
-        return myKey;
-    }
-
 }
 
 
